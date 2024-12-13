25763aba6adb20211f2c7314e0d5935062297c8a
M1ke0-0 <143715599+M1ke0-0@users.noreply.github.com>
2024-12-13 21:18:04 +0300

    Add files via upload

---
+++ LABA4.py
@@ -0,0 +1,11 @@
+print("Nomer 1")
+
+import math
+print(math.sqrt(number))
+
+import datetime
+print(datetime.datetime.now())
+
+
+print("Nomer 2")
+ import my_module #Что бы импортировать этот модуль его надо установить на пк
---
+++ my_module.py
@@ -0,0 +1,5 @@
+def func(peremenna):
+    return "Какая-то функция"
+
+def func_2(peremenna):
+    return "Какая-то функция"
